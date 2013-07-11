5743b02fad2ca17ef16d3fe5489d702164fed877
Aleks Ya <ya_al@bk.ru>
2013-07-11 17:32:09 +0400

    Написал тест для AuthorisedContentLoader

---
+++ test/ru/yaal/project/hhapi/loader/AuthorisedContentLoaderTest.java
@@ -0,0 +1,20 @@
+package ru.yaal.project.hhapi.loader;
+
+import org.junit.Test;
+import ru.yaal.project.hhapi.HhConstants;
+import ru.yaal.project.hhapi.HhToken;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class AuthorisedContentLoaderTest {
+    @Test
+    public void test() throws LoadException {
+        HhToken token = HhConstants.YABLOKOV_TOKEN;
+        IContentLoader loader = new AuthorisedContentLoader(token);
+        String content = loader.loadContent(HhConstants.ME_URL);
+        assertNotNull(content);
+        assertTrue(content.contains("яблоков"));
+        System.out.println(content);
+    }
+}
