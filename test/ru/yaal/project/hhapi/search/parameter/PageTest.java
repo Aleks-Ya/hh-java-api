a391d9d3992c6a10ab5242ad59a47c6191009e6c
Aleks Ya <ya_al@bk.ru>
2013-07-12 04:36:20 +0400

    Создал тест для Page.

---
+++ test/ru/yaal/project/hhapi/search/parameter/PageTest.java
@@ -0,0 +1,17 @@
+package ru.yaal.project.hhapi.search.parameter;
+
+import org.junit.Test;
+import ru.yaal.project.hhapi.search.SearchException;
+
+public class PageTest {
+
+    @Test
+    public void testCorrectPage() throws SearchException {
+        new Page(1);
+    }
+
+    @Test(expected = SearchException.class)
+    public void testIncorrectPage() throws SearchException {
+        new Page(0);
+    }
+}
