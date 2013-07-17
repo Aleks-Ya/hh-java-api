e699a7606bee3425cbfd6124787f2a11ccd77615
Aleks Ya <ya_al@bk.ru>
2013-07-17 22:26:59 +0400

    test исправил тест ParserProfessionalFieldTest

--- test/ru/yaal/project/hhapi/parser/ParserProfessionalFieldTest.java
+++ test/ru/yaal/project/hhapi/parser/ParserProfessionalFieldTest.java
@@ -1,32 +1,23 @@
 package ru.yaal.project.hhapi.parser;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import ru.yaal.project.hhapi.dictionary.entry.entries.professionalfield.ProfessionalFieldDictionary;
+import ru.yaal.project.hhapi.loader.FakeContentLoader;
+import ru.yaal.project.hhapi.loader.IContentLoader;
+import ru.yaal.project.hhapi.loader.UrlConstants;
 
-import ru.yaal.project.hhapi.dictionary.entry.entries.professionalfield.ProfessionalField;
+import static org.junit.Assert.assertTrue;
 
 public class ParserProfessionalFieldTest {
 
-	@Test
-	public void testParseSpecialization() throws Exception {
-		final String content = "{\"specializations\": "
-				+ "[{\"id\": \"24.493\", \"name\": \"Парикмахер\"}, "
-				+ "{\"id\": \"24.492\", \"name\": \"Массажист\"}, "
-				+ "{\"id\": \"24.624\", \"name\": \"Ногтевой сервис\"}, "
-				+ "{\"id\": \"24.379\", \"name\": \"Администрация\"}, "
-				+ "{\"id\": \"24.380\", \"name\": \"Продажи\"}, "
-				+ "{\"id\": \"24.378\", \"name\": \"Тренерский состав\"},"
-				+ "{\"id\": \"24.377\", \"name\": \"Косметология\"}], " + ""
-				+ "\"id\": \"24\", "
-				+ "\"name\": \"Спортивные клубы, фитнес, салоны красоты\"}";
-//		ProfessionalField field = IParser.MAPPER.readValue(content,
-//				ProfessionalField.class);
-//		assertNotNull(field);
-//		assertEquals("24", field.getId());
-//		assertEquals("Спортивные клубы, фитнес, салоны красоты",
-//				field.getName());
-//		assertEquals(7, field.getSpecializations().size());
-	}
+    @Test
+    public void testParseSpecialization() throws Exception {
+        IContentLoader loader = new FakeContentLoader();
+        String content = loader.loadContent(UrlConstants.SPECIALIZATIONS_URL);
+        IParser<ProfessionalFieldDictionary> parser = new ProfessionalFieldsParser();
+        ProfessionalFieldDictionary dictionary = parser.parse(content);
+        assertTrue(dictionary.hasEntryWithId("24.492"));
+        assertTrue(dictionary.hasEntryWithName("Массажист"));
+    }
 
 }
