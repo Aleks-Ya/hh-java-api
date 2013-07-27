9ee022600591c8e7f7638ddbe4c7967806a8ef74
Aleks Ya <ya_al@bk.ru>
2013-07-28 02:57:59 +0400

    feature переключил AbstractVacancySearch на shortTerm лоадер

--- src/ru/yaal/project/hhapi/search/AbstractVacancySearch.java
+++ src/ru/yaal/project/hhapi/search/AbstractVacancySearch.java
@@ -17,7 +17,7 @@ public abstract class AbstractVacancySearch<E> implements ISearch<E> {
     public static final int MIN_VACANCIES_LIMIT = PerPage.MIN_PER_PAGE;
     public static final int DEFAULT_VACANCIES_LIMIT = 20;
     protected IParser<VacancyList> parser = new VacanciesParser();
-    protected IContentLoader loader = ContentLoaderFactory.newInstanceLongTermCache();
+    protected IContentLoader loader = ContentLoaderFactory.newInstanceSortTermCache();
     protected SearchParameterBox parameterBox = new SearchParameterBox();
 
     protected void putParametersToLoader(SearchParameterBox parameterBox) {
