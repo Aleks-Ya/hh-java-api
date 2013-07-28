84814c808c24466b1dd9b61477b3638f7c8f7493
Aleks Ya <ya_al@bk.ru>
2013-07-28 06:03:19 +0400

    chore delete ITreeDictionaryEntry.java

--- src/ru/yaal/project/hhapi/dictionary/entry/ITreeDictionaryEntry.java
+++
@@ -1,7 +0,0 @@
-package ru.yaal.project.hhapi.dictionary.entry;
-
-import ru.yaal.project.hhapi.dictionary.IDictionary;
-
-public interface ITreeDictionaryEntry {
-    IDictionary<? extends IDictionaryEntry> getNested();
-}
