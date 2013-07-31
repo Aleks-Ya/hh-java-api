ff614569447749087bbd1b9ea149d6cf22040c90
Aleks Ya <ya_al@bk.ru>
2013-07-31 11:27:04 +0400

    test add TextSearchTest

--- test/ru/yaal/project/hhapi/vacancy/TextSearchTest.java
+++ test/ru/yaal/project/hhapi/vacancy/TextSearchTest.java
@@ -1,9 +1,12 @@
 package ru.yaal.project.hhapi.vacancy;
 
 import org.junit.Test;
+import ru.yaal.project.hhapi.dictionary.entry.entries.small.VacancySearchFields;
+import ru.yaal.project.hhapi.loader.LoadException;
+import ru.yaal.project.hhapi.parser.ParseException;
 import ru.yaal.project.hhapi.search.ISearch;
-import ru.yaal.project.hhapi.search.SearchException;
 import ru.yaal.project.hhapi.search.ISearchParameter;
+import ru.yaal.project.hhapi.search.SearchException;
 import ru.yaal.project.hhapi.search.parameter.Text;
 
 import static org.junit.Assert.assertNotNull;
@@ -20,6 +23,21 @@ public class TextSearchTest {
         assertNotNull(result);
         assertTrue(VacancySearchTest.WITHOUT_PARAMS_VACANCIES_COUNT > result.getFound());
         assertTrue(1000 < result.getFound());
+    }
+
+    @Test
+    public void searchMultiParams() throws SearchException, LoadException, ParseException {
+        final String searchText = "java";
+        ISearchParameter text = new Text(searchText, VacancySearchFields.DESCRIPTION, VacancySearchFields.VACANCY_NAME);
+        IterableVacancyList vacancies = new IterableVacancySearch(50)
+                .addParameter(text)
+                .search();
+        final String searchTextUpperCase = searchText.toUpperCase();
+        for (Vacancy vacancy : vacancies) {
+            String name = vacancy.getName().toUpperCase();
+            String description = vacancy.getDescription().toUpperCase();
+            assertTrue(name.contains(searchTextUpperCase) || description.contains(searchTextUpperCase));
+        }
     }
 
 }
