042090383b2cfa5e00504b94c9e9b199ec8d5ed6
Aleks Ya <ya_al@bk.ru>
2013-07-31 11:36:55 +0400

    test add EmployerTest

---
+++ test/ru/yaal/project/hhapi/dictionary/entry/entries/employer/EmployerTest.java
@@ -0,0 +1,25 @@
+package ru.yaal.project.hhapi.dictionary.entry.entries.employer;
+
+import org.junit.Test;
+import ru.yaal.project.hhapi.vacancy.IterableVacancyList;
+import ru.yaal.project.hhapi.vacancy.IterableVacancySearch;
+import ru.yaal.project.hhapi.vacancy.Vacancy;
+
+import static org.junit.Assert.assertTrue;
+
+public class EmployerTest {
+    @Test
+    public void testGetSearchParameters() throws Exception {
+        final Employer expectedEmployer1 = new Employer("1455");
+        final Employer expectedEmployer2 = new Employer("6591");
+        IterableVacancyList vacancies = new IterableVacancySearch(50)
+                .addParameter(expectedEmployer1)
+                .addParameter(expectedEmployer2)
+                .search();
+        assertTrue(vacancies.size() > 0);
+        for (Vacancy vacancy : vacancies) {
+            Employer actualEmployer = vacancy.getEmployer();
+            assertTrue(actualEmployer.equals(expectedEmployer1) || actualEmployer.equals(expectedEmployer2));
+        }
+    }
+}
