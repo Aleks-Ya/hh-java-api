ce838679779e92e5f6e4e18c0ff2d90347d01e03
Aleks Ya <ya_al@bk.ru>
2013-07-31 11:09:03 +0400

    doc Schedule

--- src/ru/yaal/project/hhapi/dictionary/entry/entries/small/Schedule.java
+++ src/ru/yaal/project/hhapi/dictionary/entry/entries/small/Schedule.java
@@ -2,13 +2,14 @@ package ru.yaal.project.hhapi.dictionary.entry.entries.small;
 
 import ru.yaal.project.hhapi.dictionary.IDictionary;
 import ru.yaal.project.hhapi.dictionary.entry.AbstractDictionaryEntry;
-import ru.yaal.project.hhapi.search.SearchException;
 import ru.yaal.project.hhapi.search.ISearchParameter;
+import ru.yaal.project.hhapi.search.SearchException;
 import ru.yaal.project.hhapi.search.SearchParamNames;
 import ru.yaal.project.hhapi.search.SearchParameterBox;
 
 /**
  * График работы.
+ * Может использоваться как параметр поиска вакансий. Допускает множественное значение ("ИЛИ").
  */
 public final class Schedule extends AbstractDictionaryEntry implements ISearchParameter {
     public static final Schedule NULL_SCHEDULE = new Schedule();
