67828a840cb60c161680f6576fce5c76c5c28dc7
Aleks Ya <ya_al@bk.ru>
2013-08-05 23:58:48 +0400

    test edit PageTest

--- test/ru/yaal/project/hhapi/vacancy/PageTest.java
+++ test/ru/yaal/project/hhapi/vacancy/PageTest.java
@@ -1,7 +1,12 @@
 package ru.yaal.project.hhapi.vacancy;
 
 import org.junit.Test;
+import ru.yaal.project.hhapi.dictionary.DictionaryException;
 import ru.yaal.project.hhapi.search.SearchException;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class PageTest {
 
@@ -13,5 +18,21 @@ public class PageTest {
     @Test(expected = SearchException.class)
     public void testIncorrectPage() throws SearchException {
         new Page(0);
+    }
+
+    @Test
+    public void testPage() throws SearchException, DictionaryException {
+        VacancyList vacancyList1 = new VacancySearch().addParameter(new Page(1)).search();
+        int pages = vacancyList1.getPages();
+        int perPage = vacancyList1.getPerPage();
+        int itemsCount = vacancyList1.getItems().size();
+
+        for (int page = 1; page <= 5; page++) {
+            VacancyList vacancyList = new VacancySearch().addParameter(new Page(page)).search();
+            assertThat(vacancyList.getPage(), is(page));
+            assertThat(vacancyList.getPages(), is(pages));
+            assertThat(vacancyList.getPerPage(), is(perPage));
+            assertThat(vacancyList.getItems(), hasSize(itemsCount));
+        }
     }
 }
