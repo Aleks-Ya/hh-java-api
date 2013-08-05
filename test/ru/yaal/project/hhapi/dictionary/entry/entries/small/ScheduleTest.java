c3cd6d114ab860173b5fcb42dba136f91e32f4f9
Aleks Ya <ya_al@bk.ru>
2013-08-05 23:22:20 +0400

    test переписал тест поиска по Schedule

--- test/ru/yaal/project/hhapi/dictionary/entry/entries/small/ScheduleTest.java
+++ test/ru/yaal/project/hhapi/dictionary/entry/entries/small/ScheduleTest.java
@@ -5,17 +5,24 @@ import ru.yaal.project.hhapi.TestHelper;
 import ru.yaal.project.hhapi.vacancy.Vacancy;
 
 import static org.hamcrest.Matchers.isOneOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class ScheduleTest {
     @Test
-    public void searchMultiParameters() throws Exception {
+    public void searchBySchedule() throws Exception {
+        final Schedule expSchedule = Schedule.SHIFT;
+        for (Vacancy vacancy : TestHelper.search(expSchedule)) {
+            assertEquals(vacancy.getSchedule(), expSchedule);
+        }
+    }
+
+    @Test
+    public void searchByMultiSchedules() throws Exception {
         final Schedule expSchedule1 = Schedule.REMOTE;
         final Schedule expSchedule2 = Schedule.SHIFT;
         for (Vacancy vacancy : TestHelper.search(expSchedule1, expSchedule2)) {
             Schedule actSchedule = vacancy.getSchedule();
-            assertTrue(actSchedule.equals(expSchedule1) || actSchedule.equals(expSchedule2));
             assertThat(actSchedule, isOneOf(expSchedule1, expSchedule2));
         }
     }
--- test/ru/yaal/project/hhapi/vacancy/VacancySearchTest.java
+++ test/ru/yaal/project/hhapi/vacancy/VacancySearchTest.java
@@ -2,25 +2,15 @@ package ru.yaal.project.hhapi.vacancy;
 
 import org.junit.Test;
 import ru.yaal.project.hhapi.dictionary.DictionaryException;
-import ru.yaal.project.hhapi.dictionary.entry.entries.small.Schedule;
 import ru.yaal.project.hhapi.search.ISearch;
 import ru.yaal.project.hhapi.search.SearchException;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class VacancySearchTest {
     public static final int WITHOUT_PARAMS_VACANCIES_COUNT = 290000;
     private ISearch<VacancyList> search = new VacancySearch();
-
-    @Test
-    public void testSchedule() throws SearchException, DictionaryException {
-        search.addParameter(Schedule.SHIFT);
-        VacancyList result = search.search();
-        assertTrue(WITHOUT_PARAMS_VACANCIES_COUNT > result.getFound());
-        assertTrue(30000 < result.getFound());
-        assertThat(result.getFound(), greaterThan(30000));
-    }
 
     @Test
     public void testPerPage() throws SearchException, DictionaryException {
