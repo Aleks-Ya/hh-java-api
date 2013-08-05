be07fba7fe427b7abe41cd12cda1fe432f7ea1a1
Aleks Ya <ya_al@bk.ru>
2013-08-05 18:15:54 +0400

    test edit создал матчер hamcrest SalaryEquals и использовал его вместо статического метода SalaryTest#assertSalary()

--- test/ru/yaal/project/hhapi/vacancy/SalaryTest.java
+++ test/ru/yaal/project/hhapi/vacancy/SalaryTest.java
@@ -1,30 +1,23 @@
 package ru.yaal.project.hhapi.vacancy;
 
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.yaal.project.hhapi.dictionary.DictionaryException;
 import ru.yaal.project.hhapi.dictionary.entry.entries.small.Currency;
 import ru.yaal.project.hhapi.search.ISearch;
-import ru.yaal.project.hhapi.search.SearchException;
 import ru.yaal.project.hhapi.search.ISearchParameter;
-import ru.yaal.project.hhapi.search.parameter.OnlyWithSalary;
+import ru.yaal.project.hhapi.search.SearchException;
 import ru.yaal.project.hhapi.search.SearchParamNames;
+import ru.yaal.project.hhapi.search.parameter.OnlyWithSalary;
 
 import static org.junit.Assert.*;
 
 public class SalaryTest {
-    //todo переписать использу€ Salary.toRur() и assertSalary()
-    public static void assertSalary(Salary expectedSalary, Salary actualSalary) throws SearchException {
-        Salary expectedSalaryRur = Salary.toRur(expectedSalary);
-        Salary actualSalaryRur = Salary.toRur(actualSalary);
-        Integer expectedValue = Integer.valueOf(expectedSalaryRur.getSearchParameters().getParameterMap().get(SearchParamNames.SALARY).get(0));
-        assertNotNull(expectedValue);
-        Integer actualTo = actualSalaryRur.getTo();
-        Integer actualFrom = actualSalaryRur.getFrom();
-        assertTrue(actualTo != null || actualFrom != null);
-        actualTo = (actualTo != null) ? actualTo : actualFrom;
-        actualFrom = (actualFrom != null) ? actualFrom : actualTo;
-        assertTrue(expectedValue >= actualFrom || expectedValue <= actualTo);
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(SalaryTest.class);
 
     @Test(expected = SearchException.class)
     public void testSalaryNotSpecified() throws SearchException, DictionaryException {
@@ -46,7 +39,7 @@ public class SalaryTest {
             System.out.println(actualSalary.getFrom());
             System.out.println(actualSalary.getTo());
             System.out.println(actualSalary.getCurrency());
-            assertSalary(expectedSalary, actualSalary);
+            assertThat(actualSalary, SalaryEquals.salaryEquals(actualSalary));
         }
     }
 
@@ -103,6 +96,45 @@ public class SalaryTest {
         assertEquals(from, salaryRur.getFrom());
         assertEquals(to, salaryRur.getTo());
         assertEquals(Currency.RUR, salaryRur.getCurrency());
+    }
+
+    public static class SalaryEquals extends TypeSafeMatcher<Salary> {
+        private Salary actualSalary;
+
+        public SalaryEquals(Salary actualSalary) {
+            this.actualSalary = actualSalary;
+        }
+
+        @Factory
+        public static org.hamcrest.Matcher<Salary> salaryEquals(Salary actualSalary) {
+            return new SalaryEquals(actualSalary);
+        }
+
+        @Override
+        public boolean matchesSafely(Salary expectedSalary) {
+            try {
+                Salary expectedSalaryRur = Salary.toRur(expectedSalary);
+                Salary actualSalaryRur = Salary.toRur(actualSalary);
+                Integer expectedValue = Integer.valueOf(expectedSalaryRur.getSearchParameters()
+                        .getParameterMap().get(SearchParamNames.SALARY).get(0));
+                assertNotNull(expectedValue);
+                Integer actualTo = actualSalaryRur.getTo();
+                Integer actualFrom = actualSalaryRur.getFrom();
+                assertTrue(actualTo != null || actualFrom != null);
+                actualTo = (actualTo != null) ? actualTo : actualFrom;
+                actualFrom = (actualFrom != null) ? actualFrom : actualTo;
+                return (expectedValue >= actualFrom || expectedValue <= actualTo);
+            } catch (SearchException e) {
+                LOG.error(e.getMessage(), e);
+                return false;
+            }
+        }
+
+        public void describeTo(Description description) {
+            description.appendText("salary not equals:");
+            description.appendValue(actualSalary);
+        }
+
     }
 
 }
