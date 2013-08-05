6e58f32690e52cf0069030b7bcf9421f40dce8cf
Aleks Ya <ya_al@bk.ru>
2013-08-05 23:42:48 +0400

    test edit PerPageTest

--- test/ru/yaal/project/hhapi/vacancy/PerPageTest.java
+++ test/ru/yaal/project/hhapi/vacancy/PerPageTest.java
@@ -1,7 +1,11 @@
 package ru.yaal.project.hhapi.vacancy;
 
 import org.junit.Test;
+import ru.yaal.project.hhapi.dictionary.DictionaryException;
+import ru.yaal.project.hhapi.search.ISearch;
 import ru.yaal.project.hhapi.search.SearchException;
+
+import static org.junit.Assert.assertEquals;
 
 public class PerPageTest {
     @Test
@@ -18,5 +22,16 @@ public class PerPageTest {
     @Test(expected = SearchException.class)
     public void testIncorrectPerPage2() throws SearchException {
         new PerPage(501);
+    }
+
+    @Test
+    public void testPerPage() throws SearchException, DictionaryException {
+        final Integer perPageCount = 5;
+        PerPage perPage = new PerPage(perPageCount);
+        ISearch<VacancyList> search = new VacancySearch();
+        search.addParameter(perPage);
+        VacancyList result = search.search();
+        assertEquals(perPageCount, result.getPerPage());
+        assertEquals((Object) perPageCount, result.getItems().size());
     }
 }
