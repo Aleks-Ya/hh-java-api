f524fec1c7e8690096d55b0bd409a4dcbd3b748a
Aleks Ya <ya_al@bk.ru>
2013-08-06 03:30:52 +0400

    test fix неверно указана временная папка для теста кэша

--- test/ru/yaal/project/hhapi/loader/cache/ClassResourceStorageTest.java
+++ test/ru/yaal/project/hhapi/loader/cache/ClassResourceStorageTest.java
@@ -14,7 +14,7 @@ public class ClassResourceStorageTest {
     @Test
     public void search() {
         ICache storage = new ClassResourceCache(
-                new File(System.getProperty(ContentLoaderFactory.JAVA_IO_TMPDIR) + "test_hh_api_cache\\test\\"));
+                new File(ContentLoaderFactory.JAVA_IO_TMPDIR + "test_hh_api_cache\\test\\"));
         String content = storage.search(UrlConstants.DICTINARIES_URL);
         assertThat(content.length(), greaterThan(100));
     }
